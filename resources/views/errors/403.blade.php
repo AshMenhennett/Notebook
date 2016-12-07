97b4e54e7dfb58f76d050b20e6531dab61e04499
Ashley Menhennett <ashleymenhennett@gmail.com>
2016-12-07 13:18:14 +1030

    Updated errors/403.blade.php

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -40,7 +40,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">403.</div>
+                <div class="title">Forbidden.</div>
             </div>
         </div>
     </body>
